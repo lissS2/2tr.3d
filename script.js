65cfca1177e9b3d30c2ab876a7b323190a6533dd
Luisa Zanardini <106076128+lissS2@users.noreply.github.com>
2024-08-21 19:53:26 -0300

    .

--- script.js
+++ script.js
@@ -65,6 +65,10 @@ function mostraAlternativas() {
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
