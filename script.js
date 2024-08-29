75ddfa6cd4d41c02b7bf42fcfc361ffb633db5ae
Luisa Zanardini <106076128+lissS2@users.noreply.github.com>
2024-08-29 09:39:50 -0300

    .

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal = "Obrigado!";
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
